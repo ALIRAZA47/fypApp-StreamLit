1cf12c150406307f40e24b896d3ddf4919b32d71
Ali Raza Khan <raza2018@namal.edu.pk>
2022-01-17 18:00:43 +0500

    Spark NLP Tab is Added and some of its functionality too

--- SparkNLP.py
+++ SparkNLP.py
@@ -1,7 +1,120 @@
+from turtle import width
 import pandas as pd
 import streamlit as st
 
-def func1():
-    df = pd.DataFrame(['Geeks', 'For', 'Geeks', 'is', 
-                'portal', 'for', 'Geeks'])
-    st.dataframe(df)
+# ## Import Libs and Data for Spark
+
+# %%
+import pandas as pd
+import numpy as np
+import re
+
+import json
+from pyspark.ml import Pipeline
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from sparknlp.annotator import *
+from sparknlp.base import *
+import sparknlp
+from sparknlp.pretrained import PretrainedPipeline
+
+
+# %%
+# tweets_df = pd.read_csv('tweets.csv')
+# tweets_df.columns = ['sentiment','tweet_id','timestamp','q','user','text']
+# print(tweets_df)
+
+
+colnames=['tweet_id', 'sentiment', 'text'] 
+# tweets_df.head()
+
+# %%
+def readAndShowData():
+    tweets_df = pd.read_csv('data/3points_dataset_tweets.tsv',sep="\t" ,encoding = "ISO-8859-1", header=None, names=colnames) 
+    tweets_df = tweets_df[(tweets_df.text != "Not Available")]
+    tweets_df = tweets_df[(tweets_df.text != "")]
+    tweets_df = tweets_df[(tweets_df.text != " ")]
+
+    tweets_df.head()
+
+    tweets_df['text'] = tweets_df['text'].apply(lambda x: re.split('http:\/\/.*', str(x))[0])
+    return tweets_df
+
+
+# %% [markdown]
+# ## Preparing and Training Model
+
+# %%
+# %%
+def startSparkAndPreparePipeline():
+    spark = sparknlp.start()
+    print("Spark NLP version", sparknlp.version())
+    print("Apache Spark version:", spark.version)
+
+    documentAssembler = DocumentAssembler()\
+        .setInputCol("text")\
+        .setOutputCol("document")
+        
+    use = UniversalSentenceEncoder.pretrained(name="tfhub_use", lang="en")\
+    .setInputCols(["document"])\
+    .setOutputCol("sentence_embeddings")
+
+
+    sentimentdl = SentimentDLModel.pretrained(name="sentimentdl_use_twitter", lang="en")\
+        .setInputCols(["sentence_embeddings"])\
+        .setOutputCol("sentiment")
+
+    nlpPipeline = Pipeline(
+        stages = [
+            documentAssembler,
+            use,
+            sentimentdl
+        ])
+    empty_ds = spark.createDataFrame([['']]).toDF("text")
+    lp = LightPipeline(nlpPipeline.fit(empty_ds))
+    return spark, nlpPipeline, lp
+
+
+# %%
+def transformData(tweets_df, spark, nlpPipeline):
+    empty_df = spark.createDataFrame([['']]).toDF("text")
+
+    pipelineModel = nlpPipeline.fit(empty_df)
+
+    df = spark.createDataFrame(pd.DataFrame({"text":tweets_df['text']}))
+    result = pipelineModel.transform(df)
+    
+    return result
+
+# %% [markdown]
+# ## Print and Writing Results
+
+# %%
+def printTransformedResultsUsingSpark(result):
+    return result.select(F.explode(F.arrays_zip('document.result', 'sentiment.result')).alias("cols")) \
+    .select(F.expr("cols['0']").alias("document"),
+            F.expr("cols['1']").alias("sentiment")).show(truncate=False)
+
+# %%
+def concateResults(result, tweets_df):
+    pd_df = result.select(F.explode(F.arrays_zip('document.result', 'sentiment.result')).alias("cols")) \
+    .select(F.expr("cols['0']").alias("document"),
+            F.expr("cols['1']").alias("sentiment")).toPandas()
+
+    pred_sentis= list(pd_df['sentiment'])
+    tweets_df.insert(3, 'Predicted Sentiments', pred_sentis)
+    # tweets_df.head()
+    return tweets_df
+    # tweets_df.to_csv('tweets_with_predicted_sentiment.csv', index=False)
+# %% 
+# @title driver function
+def doEverything():
+    spark, nlpPipeline, lightPipeline = startSparkAndPreparePipeline()
+    rawData = readAndShowData()
+    # transform data
+    result = transformData(rawData, spark, nlpPipeline)
+
+    # transform results of Spark to Pandas DataFrame
+    transformedData = concateResults(result, rawData)
+
+    return transformedData
--- nlpApp.py
+++ nlpApp.py
@@ -8,6 +8,9 @@ from afinn import Afinn
 import os
 import OpenAI
 import TextBlob
+from st_aggrid import AgGrid
+# from sparknlp.base import LighPipiLine
+
 
 
 
@@ -59,36 +62,6 @@ def customizeUI():
 # *******************************************************************
 
 # *******************************************************************
-
-def sentimentAnalysisAfinn(rawText):
-    #instantiate afinn
-    afn = Afinn()
-    score = afn.score(rawText) 
-    return score 
-
-def covent_to_dataFrameVADER(sentiDic):
-    sentiDataFrame = pd.DataFrame(sentiDic.items(), columns=['metric', 'value'])
-    return sentiDataFrame
-
-
-def sentiAnalysisVADER(rawText):
-    	# Create a SentimentIntensityAnalyzer object.
-	sid_obj = SentimentIntensityAnalyzer()
-
-	# polarity_scores method of SentimentIntensityAnalyzer
-	# object gives a sentiment dictionary.
-	# which contains pos, neg, neu, and compound scores.
-	sentiment_dict = sid_obj.polarity_scores(rawText)
-	
-	#print("Overall sentiment dictionary is : ", sentiment_dict)
-	#print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
-	#print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
-	#print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
-	#print("Sentence Overall Rated As", end = " ")
-    
-	return sentiment_dict
-
-
   
 def main():
     customizeUI()
@@ -109,58 +82,45 @@ def main():
         col1,col2,col3 = st.columns(3) 
         if submit_button:
             with col1:
-                
-                #***************************************************************
                 # Open AI Sentiment Analysis
                 OpenAI.showResults(inputText) 
-            
                 
             with col2:
-                # st.info("TextBlob Sentiment Analysis (sentiwordnet)") #TextBlob Sentiment Analysis (sentiwordnet)
-                # sentiment = TextBlob(inputText).sentiment
-                # st.write(sentiment)
-                # #Emoji
-                # if sentiment.polarity > 0:
-                #     st.markdown("**Sentiment::** Positive :smiley: ")
-                # elif sentiment.polarity < 0:
-                #     st.markdown("**Sentiment::** Negative :angry: ")
-                # else: #polarity == 0   
-                #     st.markdown("**Sentiment::** Neutral 😐 ")
-
-                # #DataFrame TextBlob Output
-                # resultDataFrame = covent_to_dataFrame(sentiment)
-                # st.dataframe(resultDataFrame)
-                # # Visualization
-                # c = alt.Chart(resultDataFrame).mark_bar().encode(
-				# 	x='metric',
-				# 	y='value',
-				# 	color='metric')
-                # st.altair_chart(c,use_container_width=True)
-                
                 TextBlob.showResults(inputText)
             
             with col3:
-                st.info("Afinn Sentiment Analysis") #TextBlob Sentiment Analysis (sentiwordnet)
-                sentiment = sentimentAnalysisAfinn(inputText)
-                st.write(sentiment)
-                #Emoji
-                if sentiment > 0:
-                    st.markdown("**Sentiment::** Positive :smiley: ")
-                elif sentiment < 0:
-                    st.markdown("**Sentiment::** Negative :angry: ")
-                else: #polarity == 0   
-                    st.markdown("**Sentiment::** Neutral 😐 ")
-
+                pass
                 
 
-    elif choice == "ML": 
-        st.subheader("ML-Based Sentiment Analysis")
-        from SparkNLP import func1
-        func1()
-               
+    elif choice == "Spark NLP": 
+        st.subheader("John Snow Labs' Spark NLP Sentiment Analysis")
+        import SparkNLP
+        rawData = SparkNLP.readAndShowData()
+        # st.header("Raw Comments (Twitter)", 
+        st.markdown('<h4 style="text-align: center;:"> \
+                    Raw Comments (Twitter) \
+                    </h4>',
+                    unsafe_allow_html=True)
+        AgGrid(rawData)
+        
+        # Analyze comments
+        sparkAnalyzeBtn = st.button("Analyze Comments")
+        if sparkAnalyzeBtn: #if the button is clicked
+            st.markdown('<h4 style="text-align: center;:"> \
+                    Resultant Data (with Predicted Sentiments) \
+                    </h4>',
+                    unsafe_allow_html=True)
+            resultDF = SparkNLP.doEverything()
+            AgGrid(resultDF)
+        
+            # Visualize Results
+            # st.markdown('<h4 style="text-align: center;:"> \
+            #             Results '+ str(resultDF.sentiment.value_counts()) +' \
+            #             </h4>',
+            #         unsafe_allow_html=True)
+        
+        
     elif choice == "Comparision": 
-        st.subheader("Comparision")
-    else:
         st.subheader("About")
         
 if __name__ == "__main__":
