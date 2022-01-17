2dfc52b4c9fb4eb5819a596f4e3de0fe2099f5e4
Ali Raza Khan <raza2018@namal.edu.pk>
2022-01-17 11:43:20 +0500

    Modularized the App

--- nlpApp.py
+++ nlpApp.py
@@ -109,20 +109,7 @@ def main():
         with st.form(key='nlpForm'):
             raw_text = st.text_area("Enter Text Here:")
             submit_button = st.form_submit_button(label='Analyze')
-                #***************************************************************
-                
-                # #Openai GPT3
-                # st.info("Openai GPT3") #Openai GPT-3 <--------------------------------------
-                # sentimentOpen,rawResponse = openaiSentiAnalysis(raw_text)
-                # st.write(rawResponse)
-                # #Emoji
-                # if sentimentOpen == "Positive":
-                #     st.markdown("**Sentiment::** Positive :smiley: ")
-                # elif sentimentOpen == "Negative":
-                #     st.markdown("**Sentiment::** Negative :angry: ")
-                # else: #polarity == 0   
-                #     st.markdown("**Sentiment::** Neutral 😐 ")
-                # #*****
+            
         # layout
         col1,col2,col3 = st.columns(3) 
         if submit_button:
