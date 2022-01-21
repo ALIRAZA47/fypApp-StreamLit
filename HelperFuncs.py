3f7946d7ce11e6f0e60be6fcb8af3332de6f760e
Ali Raza Khan <raza2018@namal.edu.pk>
2022-01-21 19:48:49 +0500

    Filters to Batch analysis dataframes added

--- HelperFuncs.py
+++ HelperFuncs.py
@@ -1,5 +1,15 @@
 import pandas as pd
+from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 
 def generateConfusionMatrix(data):
         confMatrix = pd.crosstab(data['True Sentiment'], data['Predicted Sentiment'])
+
+
+def buildGridOptionAgGrid(df):
+    gb = GridOptionsBuilder.from_dataframe(df)
+    # gb.configure_pagination()
+    gb.configure_side_bar()
+    gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc="sum", editable=True)
+    gridOptions = gb.build()
+    return gridOptions
