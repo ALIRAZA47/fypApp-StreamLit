fe64375c1ad00234e7fb824b92157d786e6b146d
Ali Raza Khan <raza2018@namal.edu.pk>
2022-01-23 17:41:06 +0500

    Live Analysis completed

--- OpenAI.py
+++ OpenAI.py
@@ -6,7 +6,7 @@ import streamlit as st
 def sentimentAnalysis(rawText):    
     openai.api_key = "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"
     trainDatePlusRawText = "This is a Sentence sentiment classifier\n\n\nSentence: \"I loved the new Batman movie!\"\nSentiment: Positive\n###\nSentence: \"I hate it when my phone battery dies.\"\nSentiment: Negative\n###\nSentence: \"My day has been 👍\"\nSentiment: Positive\n###\nSentence: \"This is the link to the article\"\nSentiment: Neutral\n###\nSentence: \"This new music video blew my mind\"\nSentiment: Positive\n###\nSentence: \""
-    trainDatePlusRawText = trainDatePlusRawText + rawText + "\"\nSentiment:"
+    trainDatePlusRawText = trainDatePlusRawText + rawText + "\"\nSentiment: "
     response = openai.Completion.create(
     model="davinci:ft-personal-2022-01-23-11-55-31",
     prompt=trainDatePlusRawText,temperature=0.3,
@@ -15,17 +15,20 @@ def sentimentAnalysis(rawText):
     frequency_penalty=0.5,
     presence_penalty=0.0,
     stop=["###"])
-    return response["choices"][0]["text"].split(' ')[1], response
+    return response["choices"][0]["text"][:-1].strip(), response
     
 def showResults(rawText):
     sentimentOpen,rawResponse = sentimentAnalysis(rawText)
     # st.write(rawResponse)  #full response from openAI API
     #Emoji
+    sentimentOpen = sentimentOpen.lower()
     if sentimentOpen == "positive":
         st.markdown("**Sentiment::** Positive :smiley: ")
-    elif sentimentOpen == "negative":
+    elif sentimentOpen == "pegative":
         st.markdown("**Sentiment::** Negative :angry: ")
-    else: #polarity == 0   
+    elif sentimentOpen == "neutral":    
         st.markdown("**Sentiment::** Neutral 😐 ")
+    else:
+        st.markdown("**Sentiment::** None 😐 ")
 
 
